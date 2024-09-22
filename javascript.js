2c7f8266283f06c09b2f0a3f134c6e3e1a9af12a
brewmaster12 <seanywg@gmail.com>
2024-09-22 12:35:11 +0100

    Imported completed assignment from laptop version

--- javascript.js
+++ javascript.js
@@ -1,31 +1,74 @@
-function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * 3)
-
-    if (computerChoice === 0) {
-        return "rock";
-    } else if (computerChoice === 1) {
-        return "paper";
-    } else if (computerChoice === 2) {
-        return "scissors";
-    }
-}
-
-
-function getHumanChoice() {
-    let humanChoice = prompt("Enter rock, paper or scissors")
-
-    if (humanChoice === "rock") {
-        return `You chose ${humanChoice}`;
-    } else if (humanChoice === "paper") {
-        return `You chose ${humanChoice}`;
-    } else if (humanChoice === "scissors") {
-        return `You chose ${humanChoice}`;
-    } else {
-        return "Invalid. Pleaser enter rock, paper or scissors";
-    }
-}
-
-console.log(getHumanChoice())
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playGame(round) {
+
+    function getHumanChoice() {
+        let humanChoice = prompt("Enter rock paper or scissors").toLowerCase();
+
+        if (humanChoice === "rock") {
+            return humanChoice;
+        } else if (humanChoice === "paper") {
+            return humanChoice;    
+        } else if (humanChoice === "scissors") {
+            return humanChoice;
+        } else { return "invalid" }
+    }
+
+    function getComputerChoice() {
+        let computerChoice = Math.floor(Math.random() * 3)
+
+        if (computerChoice === 0) {
+            return "rock"
+        } else if (computerChoice === 1) {
+            return "paper"
+        } else if (computerChoice === 2) {
+            return "scissors"
+        }
+    }
+
+    let humanSelection = getHumanChoice();
+    let computerSelection = getComputerChoice();
+    let roundResult;
+
+    function playRound(x, y) {
+
+        if (x === "rock" && y === "rock") {
+             roundResult = "draw"
+        } else if (x === "rock" && y === "paper") {
+             roundResult = "lose"
+        } else if (x === "rock" && y === "scissors") {
+             roundResult = "win"
+        } else if (x === "paper" && y === "paper") {
+             roundResult = "draw"
+        } else if (x === "paper" && y === "scissors") {
+             roundResult = "lose"
+        } else if (x === "paper" && y === "rock") {
+             roundResult = "win"
+        } else if (x === "scissors" && y === "scissors") {
+             roundResult = "draw"
+        } else if (x === "scissors" && y === "rock") {
+             roundResult = "lose"
+        } else if (x === "scissors" && y === "paper") {
+             roundResult = "win"
+        }
+
+        
+        if (roundResult === "win") {
+            humanScore++;
+        } else if (roundResult === "lose") {
+            computerScore++;
+        }
+
+        console.log(`You chose ${humanSelection}\nComputer chose ${computerSelection}\nYou ${roundResult}`)
+    }
+
+    playRound(humanSelection, computerSelection);
+
+    console.log(`Human: ${humanScore} Computer: ${computerScore}`)
+
+    if (round < 5) playGame(round + 1);
+}
+
+playGame(1);
