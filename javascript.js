2944fb57fb84fc1b38df299fedb6743b23201620
brewmaster12 <seanywg@gmail.com>
2024-10-03 17:45:28 +0100

    Add RPS buttons and config them

--- javascript.js
+++ javascript.js
@@ -1,22 +1,9 @@
 
 let humanScore = 0;
 let computerScore = 0;
+let roundResult;
 
-function playGame() {
-
-    function getHumanChoice() {
-        let humanChoice = prompt("Rock, Paper or Scissors?").toLowerCase();
-
-        if (humanChoice === "rock") {
-            return humanChoice;
-        } else if (humanChoice === "paper") {
-            return humanChoice;    
-        } else if (humanChoice === "scissors") {
-            return humanChoice;
-        } else { return "invalid" }
-    }
-
-    function getComputerChoice() {
+function getComputerChoice() {
         let computerChoice = Math.floor(Math.random() * 3)
 
         if (computerChoice === 0) {
@@ -26,53 +13,55 @@ function playGame() {
         } else if (computerChoice === 2) {
             return "scissors"
         }
+}
+
+function playRound(x, y) {
+
+    if (x === y) {
+        roundResult = "draw"
+    } else if (x === "rock" && y === "paper") {
+        roundResult = "lose"
+    } else if (x === "rock" && y === "scissors") {
+        roundResult = "win"
+    } else if (x === "paper" && y === "scissors") {
+        roundResult = "lose"
+    } else if (x === "paper" && y === "rock") {
+        roundResult = "win"
+    } else if (x === "scissors" && y === "rock") {
+        roundResult = "lose"
+    } else if (x === "scissors" && y === "paper") {
+        roundResult = "win"
     }
 
-    let humanSelection = getHumanChoice();
-    let computerSelection = getComputerChoice();
-    let roundResult;
-
-    function playRound(x, y) {
-
-        if (x === y) {
-             roundResult = "draw"
-        } else if (x === "rock" && y === "paper") {
-             roundResult = "lose"
-        } else if (x === "rock" && y === "scissors") {
-             roundResult = "win"
-        } else if (x === "paper" && y === "scissors") {
-             roundResult = "lose"
-        } else if (x === "paper" && y === "rock") {
-             roundResult = "win"
-        } else if (x === "scissors" && y === "rock") {
-             roundResult = "lose"
-        } else if (x === "scissors" && y === "paper") {
-             roundResult = "win"
-        }
-
-        
-        if (roundResult === "win") {
-            humanScore++;
-        } else if (roundResult === "lose") {
-            computerScore++;
-        }
-
-        console.log(`You chose ${humanSelection}\nComputer chose ${computerSelection}\nYou ${roundResult}`)
+    
+    if (roundResult == "win") {
+        humanScore++;
+    } else if (roundResult == "lose") {
+        computerScore++;
     }
 
-    playRound(humanSelection, computerSelection);
-
+    console.log(`You chose ${x}\nComputer chose ${y}\nYou ${roundResult}`)
     console.log(`Human: ${humanScore} Computer: ${computerScore}`)
 
-    while (humanScore < 5 && computerScore < 5) {
-        playGame();
+    if (humanScore === 5) {
+        console.log("YOU WIN!\n\nRefresh the page the play again");
+    } else if (computerScore === 5) {
+        console.log("YOU LOSE!\n\nRefresh the page the play again");
     }
 }
 
-playGame();
+const btnRock = document.querySelector("#btnRock");
+const btnPaper = document.querySelector("#btnPaper");
+const btnScissors = document.querySelector("#btnScissors");
 
-if (humanScore === 5) {
-    console.log("YOU WIN!\n\nRefresh the page the play again");
-} else if (computerScore === 5) {
-    console.log("YOU LOSE!\n\nRefresh the page the play again");
-}
+btnRock.addEventListener("click", () => {
+    playRound("rock", getComputerChoice());
+})
+
+btnPaper.addEventListener("click", () => {
+    playRound("paper", getComputerChoice());
+})
+
+btnScissors.addEventListener("click", () => {
+    playRound("scissors", getComputerChoice());
+})
